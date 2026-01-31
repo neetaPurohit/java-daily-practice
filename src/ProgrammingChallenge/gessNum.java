560e7bcc930761c26b74a0168e76fab96ea0cd6a
neeta purohit <neetapurohit56@gmail.com>
2026-01-31 20:08:04 +0530

    multiplication table using for loop

--- src/ProgrammingChallenge/gessNum.java
+++ src/ProgrammingChallenge/gessNum.java
@@ -1,5 +1,18 @@
 package ProgrammingChallenge;
 
+import java.util.Scanner;
+
+//create a program to guess number
 public class gessNum {
-    
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the number guessing game");
+        int num = 5, guess;
+
+        do{
+            System.out.print("Please guess the number between 0 to 10: ");
+            guess = sc.nextInt();
+        }while (num != guess);
+        System.out.println("You have successfully guessed the number");
+    }
 }
---
+++ src/ProgrammingChallenge/multiplication_Table.java
@@ -0,0 +1,4 @@
+package ProgrammingChallenge;
+
+public class multiplication_Table {
+}
