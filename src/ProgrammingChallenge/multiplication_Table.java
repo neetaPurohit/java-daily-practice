647334ed18d4aa595db40ae1422549be168a2d5e
neeta purohit <neetapurohit56@gmail.com>
2026-02-02 17:01:52 +0530

    multiplication table

--- src/ProgrammingChallenge/multiplication_Table.java
+++ src/ProgrammingChallenge/multiplication_Table.java
@@ -1,4 +1,16 @@
 package ProgrammingChallenge;
 
+import java.util.Scanner;
+
 public class multiplication_Table {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to find to multiplication of a number: ");
+        int num =  sc.nextInt();
+
+        for (int i=1; i<=10; i++){
+            System.out.println(num + " * " + i + " = " + i*num);
+        }
+    }
+
 }
